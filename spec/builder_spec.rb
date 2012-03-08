741b96ea43fd8b83d0c408e351d71f0ec9b3b5fe
R. Tyler Croy <tyler@monkeypox.org>
2012-03-08 11:01:13 -0800

    Add nonfunctional specs

---
+++ spec/builder_spec.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/spec_helper.rb'
+
+
+describe BaseVagrantBuilder do
+  it 'should work' do
+    true.should be true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+require 'rubygems'
+
+require File.expand_path(File.dirname(__FILE__) + "/../models/vagrant_builder.rb")
+
